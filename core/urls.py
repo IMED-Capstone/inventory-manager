4e01d45b0a21a5f312d21d1e4b511a86920e393e
Arvind Draffen <arvinddraffen@gmail.com>
2025-04-01 18:43:33 -0500

    Use default value of 1 year from current date for orders if no start and end dates are specified.

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("orders/dates/", views.order_details, name="order_details"),
     path("orders/dates/<str:start_date>/<str:end_date>/", views.order_details, name="order_details"),
     path("pagination", views.PaginationView.as_view(), name="pagination"),
     path("", views.HomePageView.as_view(), name="home"),
--- core/views.py
+++ core/views.py
@@ -6,6 +6,9 @@ from django.template import loader
 from django.views.generic.base import TemplateView
 from .models import Item
 
+import datetime
+from dateutil.relativedelta import relativedelta
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "core/home.html"
@@ -36,7 +39,7 @@ class PaginationView(TemplateView):
         context["lines"] = show_lines
         return context
     
-def order_details(request, start_date, end_date):
+def order_details(request, start_date=((datetime.datetime.today()-relativedelta(years=1)).strftime('%Y-%m-%d')), end_date=datetime.datetime.today().strftime('%Y-%m-%d')):
     orders = Item.objects.filter(po_date__range=(start_date, end_date))
     template = loader.get_template("core/order_details.html")
     context = {
