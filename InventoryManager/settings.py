817e88e78a0b596c59796ede0bc43257ca466711
Arvind Draffen <arvinddraffen@gmail.com>
2025-04-27 14:21:53 -0500

    Improve link formatting.

--- InventoryManager/settings.py
+++ InventoryManager/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
     "djmoney",
     "django_bootstrap5",
+    "django_bootstrap_icons",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
