b21c284cda13742ab15d5ef2068a1f987cf7ee5d
Arvind Draffen <arvinddraffen@gmail.com>
2025-07-31 19:28:49 -0500

    Update URL names and add Item Transactions button to homepage.

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("items/", views.ItemDetailsView.as_view(), name="item_details"),
-    path("orders/", views.OrderDetailsView.as_view(), name="order_details"),
-    path("item-transactions/", views.ItemTransactionView.as_view(), name="item-transactions"),
+    path("items/", views.ItemDetailsView.as_view(), name="item-details"),
+    path("orders/", views.OrderDetailsView.as_view(), name="order-details"),
+    path("item-transactions/", views.ItemTransactionView.as_view(), name="itemtransaction-details"),
     path("orders/export", views.export_to_excel, name="export-orders"),
-    path("orders_advanced/", views.OrderDetailsAdvancedView.as_view(), name="order_details_advanced"),
+    path("orders_advanced/", views.OrderDetailsAdvancedView.as_view(), name="order-details-advanced"),
     path("manage_inventory/", views.ManageInventoryView.as_view(), name="manage_inventory"),
     path("manage_inventory/add_remove/", views.AddRemoveItemsByBarcodeView.as_view(), name="add_remove_items_by_barcode"),
     path("pagination", views.PaginationView.as_view(), name="pagination"),
