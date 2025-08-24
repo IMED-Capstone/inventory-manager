bbae14b097ed2a79c64e0b32d5044e15a581e81a
Arvind Draffen <arvinddraffen@gmail.com>
2025-08-24 18:25:05 -0500

    Add documentation for custom template tags.

--- core/templatetags/generic_extras.py
+++ core/templatetags/generic_extras.py
@@ -1,12 +1,34 @@
+"""Provides custom filters used for views across the project."""
+
 import re
 from django import template
 
 register = template.Library()
 
 @register.filter
-def lower(value:str):
+def lower(value:str) -> str:
+    """
+    Converts a string into lowercase.
+    Used to provide lowercase text in Django template syntax in various views.
+
+    Args:
+        value (str): input string to convert
+
+    Returns:
+        str: output string in lowercase representation
+    """
     return value.lower()
 
 @register.filter
-def camel_to_words(value: str):
+def camel_to_words(value: str) -> str:
+    """
+    Converts a camel_case string into words separated by spaces (Camel Case).
+    Used to provide string manipulation of text in various views (e.g. homepage for displaying model names nicely in respective cards).
+
+    Args:
+        value (str): input string to manipulate in camel_case
+
+    Returns:
+        str: output string as capital words separated by spaces (e.g. Camel Case)
+    """
     return re.sub(r'(?<!^)(?=[A-Z])', ' ', value)
--- core/templatetags/order_details_extras.py
+++ core/templatetags/order_details_extras.py
@@ -1,7 +1,19 @@
+"""Provides custom filters for the `OrderDetails` view."""
+
 from django import template
 
 register = template.Library()
 
 @register.filter
 def get_field_value(obj, field_name):
+    """
+    Used to get the value of a field (`field_name`) from an `Order` model in the OrderDetailsView.
+
+    Args:
+        obj (_type_): The `Order` model instance.
+        field_name (_type_): The field name for which to get the corresponding value.
+
+    Returns:
+        _type_: The value of the `Order`'s field, as specified by `field_name`.
+    """
     return getattr(obj, field_name, '')
