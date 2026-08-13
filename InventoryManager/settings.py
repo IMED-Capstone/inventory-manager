1c05098939e726e5b468bffb286554cfb545ac21
Arvind Draffen <arvinddraffen@gmail.com>
2026-08-13 11:10:23 -0500

    Ensure data directory exists in container before writing.

--- InventoryManager/settings.py
+++ InventoryManager/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = Path(os.environ.get("DATA_LOCATION", default=Path.joinpath(BASE_DIR, "data")))
+DATA_DIR = Path(os.environ.get("DATA_LOCATION"))
+DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
